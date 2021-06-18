6af64fb31668574796c495beb75cc24a8867dca1
GabrieleSgroi <79085594+GabrieleSgroi@users.noreply.github.com>
2021-06-18 22:59:52 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+"""
+Setup for Hierarchical VQ-VAE
+"""
+
+from setuptools import setup, find_packages
+
+setup(
+    name='Hierarchical VQ-VAE',
+    version='0.0.0',
+    packages=['HVQVAE'],
+    install_requires=[
+       'numpy',
+       'tensorflow',
+       'matplotlib',
+       'tensorflow-probability'
+       
+    ]
+)
