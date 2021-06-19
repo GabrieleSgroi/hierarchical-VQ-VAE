c45d655b8a2d77b9500716a7514c9693d28d5bf2
GabrieleSgroi <79085594+GabrieleSgroi@users.noreply.github.com>
2021-06-19 18:54:41 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup, find_packages
 setup(
     name='Hierarchical VQ-VAE',
     version='0.0.0',
-    packages=['HVQVAE', 'HVQVAE.hvaqvae_weights'],
+    packages=['HVQVAE'],
     install_requires=[
        'numpy',
        'tensorflow',
