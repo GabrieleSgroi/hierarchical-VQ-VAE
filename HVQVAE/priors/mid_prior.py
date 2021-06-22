d9f82c9bdd7ba52b67c9fc19386f31fbfe940880
GabrieleSgroi <79085594+GabrieleSgroi@users.noreply.github.com>
2021-06-22 21:08:50 +0200

    Update mid_prior.py

--- HVQVAE/priors/mid_prior.py
+++ HVQVAE/priors/mid_prior.py
@@ -2,7 +2,8 @@ import tensorflow as tf
 from HVQVAE.hyperparameters import KT,DT, KM, DM
 from HVQVAE.custom_layers import VectorQuantizer, CBAM, GateActivation,  CausalAttentionModule, MaskedConv2D
 from HVQVAE.utils import get_codebook, codebook_from_index
-from HVQVAE.load_utils import load_mid_quantizer, load_top_quantizer
+import HVQVAE.load_utils.load_mid_quantizer as load_mid_quantizer
+import HVQVAE.load_utils.load_top_quantizer as  load_top_quantizer
 from tensorflow.keras.layers import Conv2D, Conv2DTranspose, Concatenate, BatchNormalization, Lambda,ZeroPadding2D,Cropping2D, Dropout
 from tensorflow.keras import Model, Input
 
