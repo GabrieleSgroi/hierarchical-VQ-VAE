5eb17f2f9627cfc0108f0bcf4d0b8d23c533256d
GabrieleSgroi <79085594+GabrieleSgroi@users.noreply.github.com>
2021-06-23 21:22:36 +0200

    Update load_utils.py

--- HVQVAE/load_utils.py
+++ HVQVAE/load_utils.py
@@ -34,44 +34,44 @@ def load_top_encoder():
 
 def load_mid_encoder():
     encoder=build_mid_encoder([image_shape[0]//T_reduction,image_shape[1]//T_reduction,DT],image_shape,DM,Mencoder_layers)
-    weights_dir=current_path+"/hvqvae_weights/middle_encoder.h5"
-    encoder.load_weights(weights_dir)    
+    url='https://github.com/GabrieleSgroi/hierarchical-VQ-VAE/blob/main/HVQVAE/hvqvae_weights/mid_encoder.h5?raw=true'
+    weights_dir=tf.keras.utils.get_file('top_encoder_weights',url)    
     print("Mid encoder loaded")
     return encoder
 
 def load_bot_encoder():
     encoder=build_bot_encoder([image_shape[0]//T_reduction,image_shape[1]//T_reduction,DT],[image_shape[0]//M_reduction,image_shape[1]//M_reduction,DM],image_shape, DB, Bencoder_layers)
-    weights_dir=current_path+"/hvqvae_weights/bottom_encoder.h5"
-    encoder.load_weights(weights_dir)    
+    url='https://github.com/GabrieleSgroi/hierarchical-VQ-VAE/blob/main/HVQVAE/hvqvae_weights/bot_encoder.h5?raw=true'
+    weights_dir=tf.keras.utils.get_file('top_encoder_weights',url)  
     print("Bot encoder loaded")
     return encoder
 
 def load_top_quantizer():
     quantizer=build_quantizer(T_dim,DT,KT, top_beta,level='top')
-    weights_dir=current_path+"/hvqvae_weights/top_quantizer.h5"
-    quantizer.load_weights(weights_dir)
+    url='https://github.com/GabrieleSgroi/hierarchical-VQ-VAE/blob/main/HVQVAE/hvqvae_weights/top_quantizer.h5?raw=true'
+    weights_dir=tf.keras.utils.get_file('top_encoder_weights',url)
     print("Top quantizer Loaded")
     return quantizer
 
 def load_mid_quantizer():
     quantizer=build_quantizer(M_dim,DM,KM, mid_beta,level='mid')    
-    weights_dir=current_path+"/hvqvae_weights/middle_quantizer.h5"
-    quantizer.load_weights(weights_dir)
+    url='https://github.com/GabrieleSgroi/hierarchical-VQ-VAE/blob/main/HVQVAE/hvqvae_weights/mid_quantizer.h5?raw=true'
+    weights_dir=tf.keras.utils.get_file('top_encoder_weights',url)
     print("Mid quantizer Loaded")
     return quantizer
 
 
 def load_bot_quantizer():
     quantizer=build_quantizer(B_dim,DB,KB, bot_beta, level='bot')
-    weights_dir=current_path+"/hvqvae_weights/bottom_quantizer.h5"
-    quantizer.load_weights(weights_dir)
+    url='https://github.com/GabrieleSgroi/hierarchical-VQ-VAE/blob/main/HVQVAE/hvqvae_weights/bot_quantizer.h5?raw=true'
+    weights_dir=tf.keras.utils.get_file('top_encoder_weights',url)
     print("Bot quantizer Loaded")
     return quantizer
 
 def load_decoder():
     decoder=build_decoder([T_dim[0],T_dim[1],DT],[M_dim[0],M_dim[1],DM],[B_dim[0],B_dim[1],DB], Bdecoder_layers)
-    weights_dir=current_path+"/hvqvae_weights/decoder.h5"
-    decoder.load_weights(weights_dir)
+    url='https://github.com/GabrieleSgroi/hierarchical-VQ-VAE/blob/main/HVQVAE/hvqvae_weights/decoder.h5?raw=true'
+    weights_dir=tf.keras.utils.get_file('top_encoder_weights',url)
     print("Decoder loaded")
     return decoder
 
